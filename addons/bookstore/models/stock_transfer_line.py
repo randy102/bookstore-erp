95272b9c71deb1a7b53378ac65c76f96a7178e23
randytran <welldey102@gmail.com>
2021-11-19 13:47:44 +0700

    Feat: add transfer line report

--- addons/bookstore/models/stock_transfer_line.py
+++ addons/bookstore/models/stock_transfer_line.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api
 from odoo.exceptions import UserError
+from .stock_transfer import TRANSFER_STATES, TRANSFER_TYPES
 
 
 class StockTransferLine(models.Model):
@@ -9,20 +10,28 @@ class StockTransferLine(models.Model):
 
     book_id = fields.Many2one('bs.book')
     transfer_id = fields.Many2one('bs.stock.transfer', ondelete='cascade')
+
     qty = fields.Integer(default=1)
     stock_qty = fields.Integer(related='book_id.stock_qty')
     forecasted_stock_qty = fields.Integer(compute='_compute_forecasted_qty')
     origin_stock_qty = fields.Integer(readonly=True)
     modified_stock_qty = fields.Integer(readonly=True)
+    final_stock_qty = fields.Integer(readonly=True)
+
+    state = fields.Selection(selection=TRANSFER_STATES, related='transfer_id.state')
+    date_confirmed = fields.Datetime(related='transfer_id.date_confirmed')
+    type = fields.Selection(selection=TRANSFER_TYPES, related='transfer_id.type')
 
     def update_stock_qty(self):
         for line in self:
             new_qty = line.forecasted_stock_qty
+            old_qty = line.book_id.stock_qty
             if new_qty < 0:
                 raise UserError(f'{line.book_id.name} not have enough stock!')
-            line.origin_stock_qty = line.book_id.stock_qty
+            line.origin_stock_qty = old_qty
             line.book_id.stock_qty = new_qty
-            line.modified_stock_qty = new_qty
+            line.final_stock_qty = new_qty
+            line.modified_stock_qty = new_qty - old_qty
 
     @api.depends('book_id.stock_qty', 'transfer_id.type', 'qty')
     def _compute_forecasted_qty(self):
